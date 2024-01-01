98b2a0bae6e6b3b863ba4bfc0c468c5bb3869841
Johna210 <solomonjohna21@gmail.com>
2024-01-01 13:52:20 +0300

    fixed relative path

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: {
-          100: "#111",
-          200: "#b31312",
+    content: [".**/*.{html,js}"],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    100: "#111",
+                    200: "#b31312",
+                },
+            },
+            spacing: {
+                82: "18.75rem",
+                new: "24.5625",
+                92: "37.5",
+            },
+            fontFamily: {
+                logo: ["Righteous"],
+                title: ["Nova Square"],
+                body: ["Roboto"],
+                nav: ["Open Sans"],
+            },
         },
-      },
-      spacing: {
-        82: "18.75rem",
-        new: "24.5625",
-        92: "37.5",
-      },
-      fontFamily: {
-        logo: ["Righteous"],
-        title: ["Nova Square"],
-        body: ["Roboto"],
-        nav: ["Open Sans"],
-      },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
